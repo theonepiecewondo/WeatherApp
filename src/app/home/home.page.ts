a599e03cf911298fc05205a4e75b42045ed1b6a5
theonepiecewondo <46974579+theonepiecewondo@users.noreply.github.com>
2020-10-30 16:56:36 -0700

    intilization

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  data: any;
+  test: any;
+  error: string;
+  location: any;
+  current: any;
+  condition: any;
+  constructor(private http: HttpClient) {
+    this.data = '';
+    this.error = '';
+    this.location = {
+      name: "",
+      region: "",
+      country: "",
+      lat: 0,
+      lon: 0,
+      tz_id: "",
+      localtime_epoch: 0,
+      localtime: "",
+    };
+    this.current = {
+      temp_c: 0,
+      temp_f: 0,
+    };
+    this.condition = {};
+  }
+  private prepareDataRequest(): Observable<object> {
+    const dataUrl = 'http://api.weatherapi.com/v1/current.json?key=d7ab721567be42adaee215355203010&q=83501'
+    return this.http.get(dataUrl);
+    
+  }
+  ionViewWillEnter() {
+    this.prepareDataRequest()
+      .subscribe(
+        data => {
+          var temp = JSON.stringify(data);
+          this.data = JSON.parse(temp);
+          this.location = data['location'];
+          this.current = data['current'];
+          this.current.temp_c = Math.round(this.current.temp_c);
+          this.condition = this.current['condition'];
+          console.log(data);
+          console.log(this.condition);
+          
+        },
+        err => {
+          this.error = 'An error occurred, the data could not be retrieved: Status: ${err.status}, Message: ${err.statusText}';
+        }
+      );
+      
+  }
 }
+
